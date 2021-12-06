92f2257150519fcb40e48e408b046f9e809963ca
Luiz Rosalba <luizrosalba@gmail.com>
2021-12-06 10:31:37 -0300

    feat: Adding css to highlight cols
    Adding css to highlight cols

--- src/components/Components/Layout/ResponsiveGridExample.tsx
+++ src/components/Components/Layout/ResponsiveGridExample.tsx
@@ -4,18 +4,59 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+ import './container.css';
+
 const ResponsiveGridExample = () =>{
     return (
     <>
+    
     <Container>
+    <h4>The Col lets you specify column widths across 6 
+        breakpoint sizes (xs, sm, md, lg, xl and xxl). 
+        For every breakpoint, you can specify the amount of columns to span, 
+        or set the prop to Col lg=true for auto layout widths.
+    </h4>
     <Row>
-        <Col sm={8}>sm=8</Col>
-        <Col sm={4}>sm=4</Col>
+        <Col className="bg" sm={8}>sm=8</Col>
+        <Col className="bg" sm={4}>sm=4</Col>
     </Row>
     <Row>
-        <Col sm>sm=true</Col>
-        <Col sm>sm=true</Col>
-        <Col sm>sm=true</Col>
+        <Col className="bg" sm>sm=true</Col>
+        <Col className="bg" sm>sm=true</Col>
+        <Col className="bg" sm>sm=true</Col>
+    </Row>
+    </Container>
+
+    <Container>
+    {/* Stack the columns on mobile by making one full-width and the other half-width */}
+    <h4>You can also mix and match breakpoints to create different grids depending on the screen size.
+    </h4>
+    <Row>
+        <Col className="bg" xs={12} md={8}>
+        xs=12 md=8
+        </Col>
+        <Col className="bg" xs={6} md={4}>
+        xs=6 md=4
+        </Col>
+    </Row>
+
+    {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
+    <Row>
+        <Col className="bg" xs={6} md={4}>
+        xs=6 md=4
+        </Col>
+        <Col className="bg" xs={6} md={4}>
+        xs=6 md=4
+        </Col>
+        <Col className="bg" xs={6} md={4}>
+        xs=6 md=4
+        </Col>
+    </Row>
+
+    {/* Columns are always 50% wide, on mobile and desktop */}
+    <Row>
+        <Col className="bg" xs={6}>xs=6</Col>
+        <Col className="bg" xs={6}>xs=6</Col>
     </Row>
     </Container>
     </>
--- src/components/Layout/ResponsiveGrid.tsx
+++ src/components/Layout/ResponsiveGrid.tsx
@@ -5,13 +5,7 @@ const ResponsiveGrid = () =>{
     return (
         <>
             <h1>Grid</h1>
-            <h4>The Col lets you specify column widths across 6 
-                breakpoint sizes (xs, sm, md, lg, xl and xxl). 
-                For every breakpoint, you can specify the amount of columns to span, 
-                or set the prop to 
-                Col lg=true 
-                for auto layout widths.
-            </h4>
+
             <ResponsiveGridExample />
 
         </>
