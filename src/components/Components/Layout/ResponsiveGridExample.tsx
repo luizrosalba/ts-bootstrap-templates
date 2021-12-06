10b6de7af5d6cb057edd2c501554a3d953109d60
Luiz Rosalba <luizrosalba@gmail.com>
2021-12-06 10:50:46 -0300

    feat: responsiveGrid Final
    responsiveGrid Final

--- src/components/Components/Layout/ResponsiveGridExample.tsx
+++ src/components/Components/Layout/ResponsiveGridExample.tsx
@@ -9,7 +9,7 @@ import Col from 'react-bootstrap/Col'
 const ResponsiveGridExample = () =>{
     return (
     <>
-    
+    <h3>https://react-bootstrap.github.io/layout/grid/</h3>
     <Container>
     <h4>The Col lets you specify column widths across 6 
         breakpoint sizes (xs, sm, md, lg, xl and xxl). 
@@ -40,7 +40,7 @@ const ResponsiveGridExample = () =>{
         </Col>
     </Row>
 
-    {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
+    <h4>Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop </h4>
     <Row>
         <Col className="bg" xs={6} md={4}>
         xs=6 md=4
@@ -52,14 +52,73 @@ const ResponsiveGridExample = () =>{
         xs=6 md=4
         </Col>
     </Row>
-
-    {/* Columns are always 50% wide, on mobile and desktop */}
+    <h4>Columns are always 50% wide, on mobile and desktop</h4>
     <Row>
         <Col className="bg" xs={6}>xs=6</Col>
         <Col className="bg" xs={6}>xs=6</Col>
     </Row>
     </Container>
+
+    <Container>
+    <h4>The Col breakpoint props also have a more complicated object prop form: 
+        span: number, order: number, offset: number 
+        for specifying offsets and ordering effects.</h4>
+    <Row>
+        <Col xs>First, but unordered</Col>
+        <Col xs={{ order: 12 }}>Second, but last</Col>
+        <Col xs={{ order: 1 }}>Third, but second</Col>
+    </Row>
+    </Container>
+
+    <Container>
+    <h4>
+        The order property also supports first (order: -1) and last (order: $columns+1).
+    </h4>
+    <Row>
+        <Col xs={{ order: 'last' }}>First, but last</Col>
+        <Col xs>Second, but unordered</Col>
+        <Col xs={{ order: 'first' }}>Third, but first</Col>
+    </Row>
+    </Container>
+
+    <Container>
+        <h4>or offsetting grid columns you can set an offset value or for a more general layout, use the margin class utilities.</h4>
+    <Row>
+        <Col md={4}>md=4</Col>
+        <Col md={{ span: 4, offset: 4 }}>{`md={{ span: 4, offset: 4 }}`}</Col>
+    </Row>
+    <Row>
+        <Col md={{ span: 3, offset: 3 }}>{`md={{ span: 3, offset: 3 }}`}</Col>
+        <Col md={{ span: 3, offset: 3 }}>{`md={{ span: 3, offset: 3 }}`}</Col>
+    </Row>
+    <Row>
+        <Col md={{ span: 6, offset: 3 }}>{`md={{ span: 6, offset: 3 }}`}</Col>
+    </Row>
+    </Container>
+
+    <Container>
+        <h4>Setting column widths in Row#
+    The Row lets you specify column widths across 5 breakpoint sizes (xs, sm, md, lg, xl and xxl). For every breakpoint, you can specify the amount of columns that will fit next to each other. You can also specify auto to set the columns to their natural widths.</h4>
+    <Row xs={2} md={4} lg={6}>
+        <Col>1 of 2</Col>
+        <Col>2 of 2</Col>
+    </Row>
+    <Row xs={1} md={2}>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+    </Row>
+    <Row xs="auto">
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+    </Row>
+</Container>
+
     </>
+
+
+    
 )}
 
 export default ResponsiveGridExample; 
